6f1787ac31977b7b9d20a5de01b66b9daeb71c7a
EPonyA <wgooch2000@gmail.com>
2021-02-27 16:22:40 -0600

    Error Handling

--- day_00/src/main.rs
+++ day_00/src/main.rs
@@ -1,17 +1,35 @@
 mod input;
 
+struct MyError {
+    details: &'static str,
+}
+
+const E: MyError = MyError {
+    details: "No elements sum to 2020.",
+};
+
 pub(crate) fn main() {
     println!("{:?}", input::DATA);
-    fn recurse(d: &[i32]) -> Option<(i32, i32)> {
+    fn recurse(d: &[i32]) -> Result<(i32, i32), MyError> {
         if d.len() < 2 {
-            return None;
+            return Err(E);
         }
         for i in d {
             if d[0] + i == 2020 {
-                return Some((d[0], *i));
+                return Ok((d[0], *i));
             }
         }
         recurse(&d[1..])
     }
-    println!("{:?}", recurse(input::DATA));
+    let solution = recurse(input::DATA);
+    match solution {
+        Ok(solution) => {
+            println!("The solution is {:?}", solution)
+        }
+        // Err(E) => {
+        _ => {
+            // This is probably a bad solution.
+            eprintln!("Error")
+        }
+    }
 }
