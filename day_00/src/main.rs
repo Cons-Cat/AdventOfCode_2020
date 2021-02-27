548d3333b3ac6b1877176c08339f589570ed67f0
EPonyA <wgooch2000@gmail.com>
2021-02-27 15:28:34 -0600

    Wrote Recursive Optional

--- day_00/src/main.rs
+++ day_00/src/main.rs
@@ -2,4 +2,16 @@ mod input;
 
 pub(crate) fn main() {
     println!("{:?}", input::DATA);
+    fn recurse(d: &[i32]) -> Option<(i32, i32)> {
+        if d.len() < 2 {
+            return None;
+        }
+        for i in d {
+            if d[0] + i == 2020 {
+                return Some((d[0], *i));
+            }
+        }
+        recurse(&d[1..])
+    }
+    println!("{:?}", recurse(input::DATA));
 }
