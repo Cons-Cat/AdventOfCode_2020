81ff927567cc0713872b1a55123a8132a437081d
EPonyA <wgooch2000@gmail.com>
2021-02-28 15:27:28 -0600

    Almost Working

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -6,12 +6,13 @@ pub(crate) fn main() {
     // println!("{:?}", input::DATA);
     // v should initially be 0.
     fn recurse(d: &[i32], n: usize, o: [i32; DEPTHS]) -> Option<[i32; DEPTHS]> {
-        // v is the value we are adding to to try and reach 2020.
+        // d is the data set we compare to.
         // n is the number of remaining depthes we may recurse.
         // o is the set of values we are using to compare.
-        for i in 0..n {
+        for i in 0..d.len() {
             if n == 0 {
                 if o.iter().sum::<i32>() + d[i] == 2020 {
+                    // println!("found result");
                     let mut arr: [i32; DEPTHS] = o;
                     arr[arr.len() - 1] = d[i];
                     return Some(arr);
@@ -20,7 +21,11 @@ pub(crate) fn main() {
             } else {
                 let mut arr: [i32; DEPTHS] = o;
                 arr[arr.len() - n] = d[i];
-                return recurse(&d[i..], n - 1, arr);
+                // println!("{}, {:?}", n, arr);
+                let val = recurse(&d[i + 1..], n - 1, arr);
+                if val != None {
+                    return val;
+                }
             }
         }
         return None;
