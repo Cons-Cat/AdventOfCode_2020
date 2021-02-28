dbeef77fbbd17f7d1ea50b82ecbf798ab976d546
EPonyA <wgooch2000@gmail.com>
2021-02-28 14:43:51 -0600

    Began Day_02

---
+++ day_02/src/main.rs
@@ -0,0 +1,50 @@
+mod input;
+
+struct MyError {
+    details: &'static str,
+}
+
+const E: MyError = MyError {
+    details: "No elements sum to 2020.",
+};
+
+const DEPTHS: usize = 3;
+
+pub(crate) fn main() {
+    // println!("{:?}", input::DATA);
+    // v should initially be 0.
+    fn recurse(d: &[i32], n: usize, o: [i32; DEPTHS]) -> Option<[i32; DEPTHS]> {
+        // v is the value we are adding to to try and reach 2020.
+        // n is the number of remaining depthes we may recurse.
+        // d is the set of values we can add with.
+        for i in 0..n {
+            if n == 0 {
+                if o.iter().sum::<i32>() + d[i] == 2020 {
+                    let mut arr: [i32; DEPTHS] = o;
+                    arr[arr.len() - 1] = d[i];
+                    return Some(arr);
+                }
+                continue;
+            } else {
+                let mut arr: [i32; DEPTHS] = o;
+                arr[arr.len() - n] = d[i];
+                return recurse(&d[i..], n - 1, arr);
+            }
+        }
+        return None;
+    }
+
+    let solution = recurse(input::DATA, DEPTHS, [0; DEPTHS]);
+    match solution {
+        Some(solution) => {
+            println!(
+                "The values are {:?}, and the solution is {}",
+                solution,
+                solution.iter().product::<i32>()
+            )
+        }
+        _ => {
+            eprintln!("Found no solution.")
+        }
+    }
+}
