c46278bbe669bb59147a9331eaae77958764f8d3
EPonyA <wgooch2000@gmail.com>
2021-02-28 21:05:18 -0600

    Fixed/Completed Day 2 Part 2

--- day_02/part_2/src/main.rs
+++ day_02/part_2/src/main.rs
@@ -5,20 +5,26 @@ pub fn main() {
     let mut valids: u32 = 0;
     for i in 0..DATA.len() {
         let vals: Vec<&str> = DATA[i].split(&['-', ' '][..]).collect();
-        let min = (vals[0].chars().nth(0).unwrap().to_digit(10).unwrap() - 1) as usize;
-        let max = (vals[1].chars().nth(0).unwrap().to_digit(10).unwrap() - 1) as usize;
-        let letter = vals[2].chars().nth(0).unwrap();
-        let letter_min = if vals[3].len() >= min {
-            vals[3].chars().nth(min).unwrap()
-        } else {
-            ' '
-        };
-        let letter_max = if vals[3].len() >= max {
-            vals[3].chars().nth(max).unwrap()
-        } else {
-            ' '
-        };
-        valids += (letter.eq(&letter_min) ^ letter.eq(&letter_max)) as u32;
+        // let min = (vals[0].chars().nth(0).unwrap().to_digit(10).unwrap() - 1) as usize;
+        // let max = (vals[1].chars().nth(0).unwrap().to_digit(10).unwrap() - 1) as usize;
+        let min: usize = vals[0].to_string().parse::<usize>().unwrap() - 1;
+        let max: usize = vals[1].to_string().parse::<usize>().unwrap() - 1;
+        let letter_compare = vals[2].chars().nth(0).unwrap();
+        let letter_min = vals[3].chars().nth(min).unwrap();
+        let letter_max = vals[3].chars().nth(max).unwrap();
+        println!("{:?}", vals);
+        println!(
+            "{}, {}, {}, {}, {}, compare: {} {} {}",
+            min,
+            max,
+            letter_compare,
+            letter_min,
+            letter_max,
+            letter_compare.eq(&letter_min),
+            letter_compare.eq(&letter_max),
+            letter_compare.eq(&letter_min) ^ letter_compare.eq(&letter_max)
+        );
+        valids += (letter_compare.eq(&letter_min) ^ letter_compare.eq(&letter_max)) as u32;
     }
     println!("{} passwords are valid.", valids);
 }
