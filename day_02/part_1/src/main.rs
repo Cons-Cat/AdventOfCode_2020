e5c00c8d9ba18d95c01ec4316cc422a273a9bb1c
EPonyA <wgooch2000@gmail.com>
2021-02-28 19:40:36 -0600

    Finished Part 1

--- day_02/part_1/src/main.rs
+++ day_02/part_1/src/main.rs
@@ -1,35 +1,17 @@
+use std::str::FromStr;
 mod input;
 const DATA: &[&str] = input::DATA;
 
 pub fn main() {
+    let mut valids: u32 = 0;
     for i in 0..DATA.len() {
-        // let mut cur_str: std::str::Chars = DATA[i].chars();
-        // let mut point_char: char = ' ';
-        // let mut min_str: String = String::new();
-        // while point_char != ('-') {
-        //     point_char = cur_str.next().unwrap();
-        //     min_str.push(point_char);
-        // }
-        // let min_val = min_str.parse::<i32>().unwrap();
-        // // Skip past the '-'
-        // cur_str.next();
-        // let mut max_str: String = String::new();
-        // while point_char != (':') {
-        //     point_char = cur_str.next().unwrap();
-        //     max_str.push(point_char);
-        // }
-        // let max_val = max_str.parse::<i32>().unwrap();
-        // // Skip past the ': '
-        // cur_str.next();
-        // cur_str.next();
-        // let password = String::new();
-        // while (Some(cur_str.current().un)) {
-        //     password.push(cur_str.next())
-        // }
-        // break;
         let vals: Vec<&str> = DATA[i].split(&['-', ' '][..]).collect();
-        let min = vals[0];
-        let max = vals[1];
+        let min = FromStr::from_str(vals[0]).unwrap();
+        let max = FromStr::from_str(vals[1]).unwrap();
+        let letter = vals[2].chars().nth(0).unwrap();
         let pass = vals[3];
+        let count = pass.matches(letter).count();
+        valids += (count >= min && count <= max) as u32;
     }
+    println!("{} passwords are valid.", valids);
 }
